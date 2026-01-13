6d99090f353973c1e73482d70295d230fe9dc064
DilaraByndr7 <167536085+DilaraByndr7@users.noreply.github.com>
2026-01-13 17:20:13 +0300

    Adding Line And Better Spawn

--- Assets/Scripts/FruitManager.cs
+++ Assets/Scripts/FruitManager.cs
@@ -5,31 +5,135 @@ using UnityEngine;
 public class FruitManager : MonoBehaviour
 {
     [Header(" Elements ")]
-    [SerializeField] private GameObject fruitPrefab;
+    [SerializeField] private Fruit fruitPrefab;
+    [SerializeField] private LineRenderer fruitDropLine;
+    private Fruit currentFruit;
 
+    [Header(" Settings ")]
+    [SerializeField] private Transform fruitSpawnLine;
+    private bool canControl;
+    private bool isControlling;
+
+    [Header(" Debug ")]
+    [SerializeField] private bool enableGizmos;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-      
+        canControl = true;
+        HideLine();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetMouseButtonDown(0))
-        {
+        if (canControl)
             ManagePlayerInput();
-        }
+       
     }
 
     private void ManagePlayerInput()
     {
-        Instantiate(fruitPrefab, GetClickedWorldPosition(), Quaternion.identity);
+        if (Input.GetMouseButtonDown(0))
+        {
+            MouseDownCallback();
+        }
+        else if (Input.GetMouseButton(0) )
+        {
+            if (isControlling)
+                MouseDragCallback();
+            else
+                MouseDownCallback();
+        }
+        else if (Input.GetMouseButtonUp(0) && isControlling)
+        {
+            MouseUpCallback();
+        }
+
+       
+    }
+
+    private void MouseDownCallback()
+    {
+        DisplayLine();
+        PlaceLineAtClickedPosition();
+
+        SpawnFruit();
+
+        isControlling = true;
+    }
+    private void MouseDragCallback()
+    {
+        PlaceLineAtClickedPosition();
+
+        currentFruit.MoveTo(new Vector2(GetSpawnPosition().x, fruitSpawnLine.position.y));
+    }
+    private void MouseUpCallback()
+    {
+        HideLine();
+        currentFruit.EnablePhysics();
+        
+        canControl = false;
+        StartControlTimer();
+
+        isControlling = false;
+    }
+
+    private void SpawnFruit()
+    {
+        Vector2 spawnPosition = GetSpawnPosition();
+
+        currentFruit = Instantiate(fruitPrefab, spawnPosition, Quaternion.identity);
+
     }
 
     private Vector2 GetClickedWorldPosition()
     {
         return Camera.main.ScreenToWorldPoint(Input.mousePosition);
     }
+
+    private Vector2 GetSpawnPosition()
+    {
+        Vector2 worldClickedPosition = GetClickedWorldPosition();
+        worldClickedPosition.y = fruitSpawnLine.position.y;
+        return worldClickedPosition;
+    }
+
+    private void PlaceLineAtClickedPosition()
+    {
+        fruitDropLine.SetPosition(0, GetSpawnPosition());
+        fruitDropLine.SetPosition(1, GetSpawnPosition() + Vector2.down * 15);
+    }
+
+    private void HideLine()
+    {
+        fruitDropLine.enabled = false;
+    }
+    private void DisplayLine()
+    {
+        fruitDropLine.enabled = true;
+    }
+
+    private void StartControlTimer()
+    {
+        Invoke("StopControlTimer", 1);
+    }
+
+    private void StopControlTimer()
+    {
+        canControl = true;
+    }
+
+#if UNITY_EDITOR
+
+    private void OnDrawGizmos()
+    {
+        if (!enableGizmos) 
+            return;
+
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawLine(new Vector3(-50, fruitSpawnLine.position.y, 0), new Vector3(50, fruitSpawnLine.position.y, 0));
+    }
+
+#endif
 }
